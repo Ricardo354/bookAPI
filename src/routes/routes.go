f0a203f170875814182d6f1970a0abce776be18c
Ricardo354 <yellow_richard@proton.me>
2025-03-06 23:54:36 -0300

    routes

---
+++ src/routes/routes.go
@@ -0,0 +1,30 @@
+package routes
+
+import (
+	"livroAPI/src/handler"
+	"github.com/gofiber/fiber/v2"
+
+)
+
+//JWT auth	
+//Rate limiting
+//godotenv
+//docker
+// unit testing
+// fiber.requestcode
+
+func LivroRoutes(app *fiber.App) {
+
+	book_endpoints := app.Group("/livro")
+
+	book_endpoints.Get("/", handler.GetAllLivros)
+	book_endpoints.Get("/:id", handler.GetLivroByID)
+	book_endpoints.Post("/", handler.CreateLivro)
+	book_endpoints.Put("/:id", handler.UpdateLivroByID)
+	book_endpoints.Delete("/:id", handler.DeleteLivroByID)
+
+	// auth_endpoints := app.Group("/auth")
+
+	
+
+}
